303d7bc26c17e72f8c66e471109fce96e179c22b
Sharpz7 <adam.mcarthur62@gmail.com>
2022-10-06 00:48:04 +0000

    Trying new changes

--- buildfiles/start.sh
+++ buildfiles/start.sh
@@ -4,4 +4,8 @@
 VOUCH_DOMAINS=$(echo $VOUCH_DOMAINS | sed 's/,/ /g')
 sed -i "s/XXXXX/$VOUCH_DOMAINS/g" /sharpnet/nginx.conf
 
+# domain.com becomes VouchCookie_domain_com
+VOUCH_COOKIE_NAME=$(echo $VOUCH_COOKIE_NAME | sed 's/\./_/g')
+VOUCH_COOKIE_NAME="VouchCookie_$VOUCH_COOKIE_NAME"
+
 /vouch-proxy
